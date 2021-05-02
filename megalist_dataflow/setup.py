a82a6e7eaacb7f4dd5965342cc7ec54ad69fbdf7
Joaquim Neto <jsn.piata@gmail.com>
2021-05-02 19:34:07 -0300

    perf: :arrow_up: Update dependencies
    pytest-cov, pytz, aiohttp, six and terraform.yml

--- megalist_dataflow/setup.py
+++ megalist_dataflow/setup.py
@@ -22,6 +22,6 @@ setuptools.setup(
     url='https://cse.googlesource.com/solutions/megalist',
     install_requires=['googleads==24.1.0', 'google-api-python-client==1.10.0',
                       'google-cloud-core==1.3.0', 'google-cloud-bigquery==1.26.0',
-                      'google-cloud-datastore==1.13.1', 'aiohttp==3.6.2'],
+                      'google-cloud-datastore==1.13.1', 'aiohttp==3.7.4'],
     packages=setuptools.find_packages(),
 )
